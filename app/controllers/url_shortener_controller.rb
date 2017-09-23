4ca289c3f08de6074f8640f7e51a2407ac26ebac
Pedro De Ona <pdeon003@fiu.edu>
2017-09-22 22:59:30 -0400

    fix refactor bug

--- app/controllers/url_shortener_controller.rb
+++ app/controllers/url_shortener_controller.rb
@@ -9,7 +9,7 @@ class UrlShortenerController < ApplicationController
     @url = Url.new(url_shortener_params)
     @url.shortened_uri = short_url
     if @url.save
-      render 'show_link'
+      render 'show_link', url: @url
     else
       flash[:danger] = 'That was an invalid URI.'
       render 'new'
