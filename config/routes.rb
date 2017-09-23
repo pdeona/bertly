5d61c76a8f188aeb8c70076d907062a20e502243
Pedro De Ona <pdeon003@fiu.edu>
2017-09-22 21:48:46 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get root to: 'url_shortener#new'
+  root to: 'url_shortener#new'
 
   post '/', to: 'url_shortener#create'
 
