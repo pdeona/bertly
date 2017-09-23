dd74cd665214b737b47dbc215daf0111608bb45f
Pedro De Ona <pdeon003@fiu.edu>
2017-09-23 18:59:59 -0400

    update gems

--- app/controllers/url_shortener_controller.rb
+++ app/controllers/url_shortener_controller.rb
@@ -1,18 +1,13 @@
 class UrlShortenerController < ApplicationController
   include UrlShortenerHelper
 
+  before_action :get_long_uri, only: :create
+
   def new
     @url = Url.new
   end
 
   def create
-    if params.include? :url_shortener
-      @url = Url.new(url_shortener_params)
-    else
-      uri_string = params[:protocol] + '//' + params[:uri_domain] + '.' + params[:uri_substring]
-      @url = Url.new(uri_string: uri_string)
-      p @url
-    end
     @url.shortened_uri = short_url
     if @url.save
       render 'show_link', url: @url
@@ -28,6 +23,15 @@ class UrlShortenerController < ApplicationController
   end
 
   private
+
+  def get_long_uri
+    if params.include? :url_shortener
+      @url = Url.new(url_shortener_params)
+    else
+      uri_string = params[:protocol] + '//' + params[:uri_domain] + '.' + params[:uri_substring]
+      @url = Url.new(uri_string: uri_string)
+    end
+  end
 
   def url_shortener_params
     params.require(:url_shortener).permit(:uri_string)
